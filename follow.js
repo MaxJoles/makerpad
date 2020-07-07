24496226788ddf2550653407f3f60d5366d70f6a
Max Joles <mpjoles@gmail.com>
2020-07-07 11:17:51 -0400

    Create follow.js

---
+++ follow.js
@@ -0,0 +1,33 @@
+// Follow products
+const itemSlug = '{{wf {&quot;path&quot;:&quot;slug&quot;,&quot;type&quot;:&quot;PlainText&quot;\} }}';
+MemberStack.onReady.then(async function (member) {
+  if (member.loggedIn) {
+    const metadata = await member.getMetaData();
+    metadata.followList = metadata.followList || [];
+    const followNum = metadata.followList.length || 0;
+    $('.cc-follow-count').text(followNum);
+
+    // show/hide watchlist buttons
+    if (metadata.followList.indexOf(itemSlug) === -1) {
+      $('.cc-follow-product.cc-checked').hide();
+      console.log("not following");
+    }
+    else {
+      console.log("following");
+      $('.cc-follow-product.cc-unchecked').hide();
+    }
+
+
+    $('.cc-follow-product.cc-unchecked').click(function () {
+      if (metadata.followList.indexOf(itemSlug) === -1) {
+        metadata.followList.push(itemSlug);
+        const followNum = metadata.followList.length || 0;
+        metadata.followitemsNum = followNum;
+        member.updateMetaData(metadata);
+        $('.cc-follow-count').text(followNum);
+        $('.cc-follow-product.cc-checked').show();
+        $('.cc-follow-product.cc-unchecked').hide();
+      }
+    });
+  }
+});
